fe87b3337c895e2fa26fe85418b9dc80f023d7b9
milovanov <milovanovkostantin@gmail.com>
2020-05-11 14:46:46 +0300

    Добавлен чекбокс и красивая прокрутка

--- todolist/src/Views/Pages/Todos/TodoList/TodoList.js
+++ todolist/src/Views/Pages/Todos/TodoList/TodoList.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import classes from './TodoList.module.css';
 import Task from './Task/Task';
+import ScrollArea from 'react-scrollbar';
 
 const TodoList = () => {
     return (
     <div className={classes["ListBox"]}>
-        <Task/>
-        <Task/>
-        <Task/>
-        <Task/>
+        <ScrollArea
+         smoothScrolling={true}
+         style={{paddingRight:"10px"}}
+        >
+            <Task/>
+            <Task/>
+            <Task/>
+            <Task/>
+        </ScrollArea>
+        <div className={classes["only-me"]}>
+            <input name="me" type="checkbox" className={classes["check"]}/>
+            <label for="me" className={classes["label"]}>only me</label>
+        </div>
     </div>
     );
 };
